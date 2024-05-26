4e09ab622365f7f97a0d155393e1055c319ead17
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-05-26 19:56:11 +0530

    commit

--- NeuralNet/GPT/train.py
+++ NeuralNet/GPT/train.py
@@ -10,6 +10,7 @@ eval_interval = 300
 learning_rate = 1e-2
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 eval_iters = 200
+n_embd = 32
 # ------------------------------------------------------------------------------
 
 torch.manual_seed(1337)
@@ -62,12 +63,14 @@ torch.manual_seed(1337)
 
 class BigramLanguageModel(nn.Module):
     
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
     
     def forward(self, idx, targets = None):
-        logits = self.token_embedding_table(idx)
+        tok_emb = self.token_embedding_table(idx) # (B, T, C)
+        logits = self.lm_head(tok_emb) # (B, T, vocab_size)
         
         if targets is None:
             loss = None
@@ -94,7 +97,7 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim = 1) # (B, T+1)
         return idx
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel()
 m = model.to(device)
 
 # create a PyTorch optimizer
