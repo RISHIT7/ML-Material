ac5b1cdb609d247cebdf48bfd5a68c0b5192d4cb
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-06-07 03:20:37 +0530

    Update langchain_helper.py

--- GenAI/Langchain/RestaurantNameGenerator/langchain_helper.py
+++ GenAI/Langchain/RestaurantNameGenerator/langchain_helper.py
@@ -1,39 +1,42 @@
-from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain.chains import SequentialChain
+from langchain_huggingface.llms import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, pipeline, AutoTokenizer
 
-import os
-os.environ["OPENAI_API_KEY"] = "sk-7j6askdjhfjsudfjsdfnjsdl"
-
-llm = OpenAI(temperature=0.5)
+# model using model_id
+model_id = "microsoft/Phi-3-mini-4k-instruct"
+model = HuggingFacePipeline.from_model_id(
+    model_id = model_id,
+    task = "text-generation",
+    device = 0,
+    model_kwargs={"temperature": 0.5, "do_sample": True},
+    pipeline_kwargs={"max_new_tokens": 500}
+)
 
 def generate_restaurant_name_and_items(cuisine):
     # Chain 1: Restaurant Name
     prompt_template_name = PromptTemplate(
         input_variables = ['cuisine'],
-        template = "I want to open a new restaurant for {cuisine} cuisine. Can you suggest a name for my restaurant?"
+        template = "I want to open a new restaurant for {cuisine} cuisine. Can you suggest ONLY ONE name for my restaurant?, Give only name, nothing else!"
     )
     
-    name_chain = LLMChain(llm, prompt_template_name, output_key = "restaurant_name")
+    chain = prompt_template_name | model
+    name = chain.invoke({'cuisine': cuisine})
     
     # Chain 2: Menu items
     prompt_template_name = PromptTemplate(
         input_variables= ['restaurant_name'],
-        template = "Suggest some menu items for {restaurant_name}. Return it as a comma seperated string"
+        template = "Suggest some menu items for {restaurant_name} No sentence, only name. Return it as a comma seperated string"
     )
     
-    food_item_chain = LLMChain(llm, prompt_template_name, output_key = "menu_items")
+    chain = prompt_template_name | model
+    menu_items = chain.invoke({'restaurant_name': cuisine})
     
-    chain = SequentialChain (
-        [name_chain, food_item_chain],
-        input_variables = ['cuisine'],
-        output_variables = ['restaurant_name', 'menu_items']
-    )
-    
-    response = chain({'inputs': {'cuisine': cuisine}})
-    
-    return response
+    return {
+        'restaurant_name': name,
+        'menu_items': menu_items
+    }
 
 if __name__ == "__main__":
-    print(generate_restaurant_name_and_items("Indian"))
+    print(generate_restaurant_name_and_items("Indian")['restaurant_name'])
+    print()
+    print(generate_restaurant_name_and_items("Indian")['menu_items'])
